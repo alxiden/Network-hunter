34e74473f635ce0390ff9c22e9d33859b65ee419
Daniel <danielben-1@hotmail.com>
2024-07-06 19:39:52 +0100

    Major network changes

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import tkinter.messagebox as tkmessagebox
 import re
 import threading
 import csv
+import datetime
 
-class Nethunter:
+class Nethunter: 
     def __init__(self, root):
         self.root = root
         
@@ -31,16 +32,6 @@ class Nethunter:
         self.netentry = tk.Entry(self.frame)
         self.netentry.grid(row=0, column=1, padx=10, pady=10)
 
-        self.sublabel = tk.Label(self.frame, text="Subnet:", bg='light blue')
-        self.sublabel.grid(row=1, column=0, padx=10, pady=10)
-        self.subentry = tk.Entry(self.frame)
-        self.subentry.grid(row=1, column=1, padx=10, pady=10)
-
-        self.devlabel = tk.Label(self.frame, text="Device:", bg='light blue')
-        self.devlabel.grid(row=2, column=0, padx=10, pady=10)
-        self.deventry = tk.Entry(self.frame)
-        self.deventry.grid(row=2, column=1, padx=10, pady=10)
-
         self.scan_type = tk.StringVar(value="network")
 
         self.network_radio = tk.Radiobutton(self.frame, text="Network Scan", variable=self.scan_type, value="network", bg='light blue')
@@ -50,33 +41,39 @@ class Nethunter:
         self.port_radio.grid(row=3, column=1)
 
 
-        self.button = tk.Button(self.frame, text="Scan", command=lambda: self.scan_network(self.netentry.get(),
-                                                                                         self.subentry.get(),
-                                                                                         self.deventry.get()))
+        self.button = tk.Button(self.frame, text="Scan", command=lambda: self.scan_network(self.netentry.get()))
         self.button.grid(row=4, column=0, columnspan=2)
 
         self.action = tk.Text(self.frame, height=1, width=30, bg='light blue', bd=0, wrap=tk.WORD)
         self.action.grid(row=5, column=0, columnspan=2)
 
 
-    def scan_network(self, netID, subnet, deviceID):
+    def scan_network(self, netID):
 
         #print(netID, subnet, deviceID)
 
-        Network = netID + "." + subnet
+        self.Network = netID
 
-        regpattern = r'\b\d{3}\.\d{3}\b'
-        if netID == "" or netID == None or not re.match(regpattern, Network):
-            tkmessagebox.showerror("Error", "Please enter a valid network ID")
+        Subnetscan = r'\b\d{1,3}\.\d{1,3}\b'
+        netscan = r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\b'
+        devicescan = r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
+        portscan = r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\:\d{1,5}\b'
+
+        if self.scan_type == 'port' and re.match(portscan, self.Network):
+            portscan()
             return
+        
+        if re.match(devicescan, self.Network):
+            #print("device Scan")
+            self.devicescan()
+        elif re.match(netscan, self.Network):
+            #print("network Scan")
+            self.Netscan()
+        elif re.match(Subnetscan, self.Network):
+            #print("Subnet Scan")
+            self.Subnetscan()
         else:
-            if self.scan_type.get() == "network":
-                if deviceID == "" or deviceID == None:
-                    self.Netscan()
-                else:
-                    self.devicescan()
-            else:
-                self.portscan()
+            tkmessagebox.showerror("Error", "Invalid IP address")
        
     
     def Netscan(self):
@@ -87,7 +84,7 @@ class Nethunter:
         try:
             clients_list = []
             for i in range(1, 255):
-                ip = f'{self.netentry.get()}.{self.subentry.get()}.{str(i)}'
+                ip = f'{self.Network}.{str(i)}'
                 self.action.delete(1.0, tk.END)
                 self.action.insert(tk.END, f"Scanning {ip}")
                 self.action.update_idletasks()
@@ -98,8 +95,11 @@ class Nethunter:
                 for element in answered_list:
                     client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
                     clients_list.append(client_dict)
+
+            now = datetime.datetime.now()
+            date = now.strftime("%Y%m%d%H%M%S")
                     
-            with open('clients_list.csv', 'w', newline='') as file:
+            with open(f'clients_list{str(date)}.csv', 'w', newline='') as file:
                 writer = csv.DictWriter(file, fieldnames=["ip", "mac"])
                 writer.writeheader()
                 writer.writerows(clients_list)
@@ -111,18 +111,20 @@ class Nethunter:
             print(f"An error occurred: {e}")
 
     def devicescan(self):
-        ip = self.netentry.get() + "." + self.subentry.get() + "." + self.deventry.get()
-        arp_request = scapy.ARP(pdst=ip)
+        arp_request = scapy.ARP(pdst=self.Network)
         broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
         arp_request_broadcast = broadcast/arp_request
-        answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+        answered_list = scapy.srp(arp_request_broadcast, timeout=3, verbose=False)[0]
 
         clients_list = []
         for element in answered_list:
             client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
             clients_list.append(client_dict)
-        
-        with open('clients_list.csv', 'w', newline='') as file:
+
+        now = datetime.datetime.now()
+        date = now.strftime("%Y%m%d%H%M%S") 
+
+        with open(f'clients_list{str(date)}.csv', 'w', newline='') as file:
             writer = csv.DictWriter(file, fieldnames=["ip", "mac"])
             writer.writeheader()
             writer.writerows(clients_list)
@@ -137,7 +139,6 @@ class Nethunter:
         scan_thread.start()
 
     def run_port_scan(self):
-        ip = self.netentry.get() + "." + self.subentry.get() + "." + self.deventry.get()
         open_ports = []
         try:
             for port in range(1, 1025):
@@ -146,22 +147,52 @@ class Nethunter:
                 self.action.insert(tk.END, f"Scanning Port: {port}")
                 self.action.update_idletasks()
                 s.settimeout(1)
-                result = s.connect_ex((ip, port))
+                result = s.connect_ex((self.Network, port))
                 if result == 0:
                     open_ports.append(port)
                     #print(f"Port {port} is open")
                 s.close()
+
+            now = datetime.datetime.now()
+            date = now.strftime("%Y%m%d%H%M%S")
             
-            with open('open_ports.csv', 'w', newline='') as file:
+            with open(f'open_ports{str(date)}.csv', 'w', newline='') as file:
                 writer = csv.writer(file)
                 writer.writerow(open_ports)
 
             self.action.delete(1.0, tk.END)
-            self.action.insert(tk.END, f"Scanning complete. Results saved to open_ports.csv")
+            self.action.insert(tk.END, f"Scanning complete")
             self.action.update_idletasks()
         
         except Exception as e:
             print(f"An error occurred: {e}")
+
+    def run_subnet_scan(self):
+        arp = scapy.ARP(pdst=self.Network)
+        ether = ether(dst="ff:ff:ff:ff:ff:ff")
+        packet = ether/arp
+
+        result = scapy.srp(packet, timeout=3, verbose=0)[0]
+
+        devices = []
+
+        for sent, received in result:
+            devices.append({'ip': received.psrc, 'mac': received.hwsrc})
+
+        now = datetime.datetime.now()
+        date = now.strftime("%Y%m%d%H%M%S")
+            
+        with open(f'Subnets{str(date)}.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(devices)
+
+        self.action.delete(1.0, tk.END)
+        self.action.insert(tk.END, f"Scanning complete")
+        self.action.update_idletasks()
+
+    def Subnetscan(self):
+        scan_thread = threading.Thread(target=self.run_scan)
+        scan_thread.start()
 
 if __name__ == "__main__":
     root = tk.Tk()
